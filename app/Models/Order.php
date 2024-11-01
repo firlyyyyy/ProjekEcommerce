e70f3e64a9bde33d1f1a6716430af9b6562827bb
firlyyyyy <firlyalfaridzi88@gmail.com>
2024-11-01 14:46:40 +0800

    category resource dan brand resource done

---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'users_id',
+        'grand_total',
+        'payment_method',
+        'payment_status',
+        'status',
+        'currency',
+        'shipping_amount',
+        'shipping_method',
+        'notes'
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
+}
